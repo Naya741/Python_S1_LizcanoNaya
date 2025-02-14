3090e12061f9115894e4c7060fed1a5208054b64
Naya741 <Nayalizcanjaimes@gmail.com>
2025-02-14 07:57:06 -0500

    cambio 1

---
+++ Ejercicio listas Dia 3/listas.py
@@ -0,0 +1,8 @@
+def inicio ():
+ print("/nBienvenido al programa listas de estudiantes")
+print ("¿Que te gustaria hacer?")
+print ("1. Agregar estudiantes")
+print ("2. Ver estudiante ")
+print ("3.Editar estudiante")
+print ("4. Eliminar estudiante")
+print (" 5. Salir del programa")
